456517eaafa472bf29b5a163e7ca43f110bcc76c
opowell <opowell@gmail.com>
2025-05-02 22:45:33 +0200

    Initial commit

---
+++ src/components/TheContent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <section>
+    SECTION
+    <div>DIV</div>
+    <span>SPAN</span>
+    <input value="INPUT" />
+    <button>BUTTON</button>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/components/header/TheViews.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps<{
+  msg: string
+}>()
+const itemTypes = ref([
+  {
+    id: 'items',
+    label: 'Items',
+  },
+  {
+    id: 'itemTypes',
+    label: 'Item types',
+  },
+  {
+    id: 'colors',
+    label: 'Colors',
+  },
+])
+</script>
+
+<template>
+  <div v-for="itemType in itemTypes" :key="itemType.id">
+    {{ itemType.label }}
+  </div>
+</template>
+
+<style scoped></style>
