47d742057584045c3b088bbd67cd42a6c04a2580
opowell <opowell@gmail.com>
2025-10-11 21:54:01 +0200

    call

--- view/App.vue
+++ view/App.vue
@@ -38,9 +38,48 @@ watch(
     immediate: true,
   },
 )
+function fetchBrickLink() {
+  console.log('bl')
+  document.dispatchEvent(
+    new CustomEvent('bzClientToServer', {
+      // detail: {
+      //   url: 'https://www.bricklink.com/browse.asp',
+      //   options: {},
+      //   type: 'text'
+      // },
+      detail: {
+        url: 'https://www.bricklink.com/catalogDownload.asp?a=a',
+        options: {
+          "headers": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "max-age=0",
+            "content-type": "application/x-www-form-urlencoded",
+            "priority": "u=0, i",
+            "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1"
+          },
+          "referrer": "https://www.bricklink.com/catalogDownload.asp",
+          "body": "viewType=0&itemType=S&selYear=Y&selWeight=Y&selDim=Y&itemTypeInv=S&itemNo=&downloadType=T",
+          "method": "POST",
+          "mode": "cors",
+          "credentials": "include"
+        },
+        type: 'text'
+      },
+    }),
+  )
+}
 </script>
 <template>
   <TheHeader />
+  <button @click="fetchBrickLink">BrickLink</button>
   <TheContent />
   <PulseMonitor />
 </template>
