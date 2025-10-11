4c3a72b59dab943366d559195ee46b8f2ef34b6b
opowell <opowell@gmail.com>
2025-10-11 02:27:53 +0200

    clean up

--- borderify/background.js
+++
@@ -1,5 +0,0 @@
-console.log('background')
-
-browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  console.log('got message', message, sender, sendResponse)
-})
--- borderify/borderify.js
+++
@@ -1,6 +0,0 @@
-console.log('content script')
-document.body.style.border = "5px solid red";
-function foo() {
-  console.log('Hello from background')
-}
-foo()
