e735e4c2899b15a4b3244c23867894e0856538e3
opowell <opowell@gmail.com>
2025-10-20 00:00:09 +0200

    bump db version to get latest index

--- idb/idb.ts
+++ idb/idb.ts
@@ -7,7 +7,7 @@ import { createIndex, createStore } from './db'
 let dbConnection: IDBPDatabase | undefined
 
 const DB_NAME = 'brickzuke'
-const DB_VERSION = 10
+const DB_VERSION = 11
 
 export async function getDbConnection(): Promise<IDBPDatabase> {
   if (dbConnection) {
